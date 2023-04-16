a869dd809117604254053b85fac62516f2c217c7
Pilmo <pilmokim99@gmail.com>
2023-04-16 16:49:11 +0900

    improve ui

--- Project3(Text_Dungeon_game)/src/main.rs
+++ Project3(Text_Dungeon_game)/src/main.rs
@@ -10,10 +10,8 @@ enum Event {
         monster : loader::Monster
     },
     Shop,
-    Special,
     Rest
 }
-
 //메인 함수 시작
 fn main() {
     let mut rng = rand::thread_rng();
@@ -21,11 +19,11 @@ fn main() {
     
     // 이벤트를 결정하기 위해 벡터를 생성하여, 랜덤으로 이벤트를 삽입합니다.
     let mut events = vec![];
-    let num_events = rng.gen_range(5..=10);
+    let num_events = rng.gen_range(3..=5);
 
     // for 반복문과 match 함수를 통해 각 이벤트를 랜덤으로 삽입합니다.
     for _ in 0..num_events {
-        let event_type = rng.gen_range(0..4);
+        let event_type = rng.gen_range(0..3);
         let event = match event_type {
             0 => {
                 let monster = loader::Monster::from("./src/monster.txt");
@@ -34,8 +32,7 @@ fn main() {
                 }
             }
             1 => Event::Shop,
-            2 => Event::Special,
-            3 => Event::Rest,
+            2 => Event::Rest,
             _ => unreachable !()
         };
         events.push(event);
@@ -55,6 +52,7 @@ fn main() {
             Event::Combat {                         // 몬스터와 만났을 때
                 monster
             } => {
+                println!("");
                 println !("{}를 만났습니다!", monster.name);
                 loop {
                     println !("플레이어 HP: {}, 골드: {}", player.hp, player.gold);
@@ -68,10 +66,10 @@ fn main() {
                     io::stdin()
                         .read_line(& mut input)
                         .unwrap();
-
+                    
                     if input.trim() == "공격" {
                         println!("+------------------------------------+");
-                        let damage = rng.gen_range(5..=15);
+                        let damage = rng.gen_range(50..=100);
                         println !("플레이어가 {}의 피해를 입혔습니다!", damage);
                         monster.hp -= damage;
                         println !("몬스터의 체력: {}", monster.hp);
@@ -79,6 +77,7 @@ fn main() {
                         if monster.hp <= 0 {
                             println !("몬스터를 물리쳤습니다!");
                             player.gold += monster.gold;
+                            println!("+------------------------------------+");
                             break;
                         }
 
@@ -91,6 +90,7 @@ fn main() {
                         }
                     } else if input.trim() == "도망" {
                         println !("플레이어가 도망쳤습니다!");
+                        println!("+------------------------------------+");
                         break;
                     } else {
                         println !("잘못된 입력입니다. 다시 입력해주세요.");
@@ -100,63 +100,64 @@ fn main() {
                 }
             }
             Event::Shop => { //상점에서
+                println!("");
                 println !("상점을 발견했습니다!");
-                println !("현재 골드: {}", player.gold);
-                println !("무기: {}, 가격: 50", player.weapon);
-                println !("방어구: {}, 가격: 50", player.armor);
-                print !("구매할 아이템을 선택해주세요. (무기/방어구/나가기) ");
-                io::stdout()
-                    .flush()
-                    .unwrap();
+                loop{
+                    println !("현재 골드: {}", player.gold);
+                    println !("현재 무기: {} || (판매중인 무기: 활, 가격: 50)", player.weapon);
+                    println !("현재 방어구: {} || (판매중인 방어구: 갑옷 가격: 50)", player.armor);
+                    print !("구매할 아이템을 선택해주세요. (무기/방어구/나가기) ");
+                    io::stdout()
+                        .flush()
+                        .unwrap();
 
-                let mut input = String::new();
-                io::stdin()
-                    .read_line(& mut input)
-                    .unwrap();
+                    let mut input = String::new();
+                    io::stdin()
+                        .read_line(& mut input)
+                        .unwrap();
 
-                if input.trim() == "무기" {
-                    if player.gold >= 50 {
-                        println !("무기를 구매했습니다!");
-                        player.weapon = String::from("활");
-                        player.gold -= 50;
+                    if input.trim() == "무기" {
+                        if player.gold >= 50 {
+                            println !("무기를 구매했습니다!");
+                            player.weapon = String::from("활");
+                            player.gold -= 50;
+                        } else {
+                            println !("골드가 부족합니다!");
+                        }
+                    } else if input.trim() == "방어구" {
+                        if player.gold >= 50 {
+                            println !("방어구를 구매했습니다!");
+                            player.armor = String::from("갑옷");
+                            player.gold -= 50;
+                        } else {
+                            println !("골드가 부족합니다!");
+                        }
+                    } else if input.trim() == "나가기" {
+                        println !("상점에서 나왔습니다.");
+                        println!("#------------------------------------#");
+                        break;
                     } else {
-                        println !("골드가 부족합니다!");
+                        println !("잘못된 입력입니다. 다시 입력해주세요.");
                     }
-                } else if input.trim() == "방어구" {
-                    if player.gold >= 50 {
-                        println !("방어구를 구매했습니다!");
-                        player.armor = String::from("갑옷");
-                        player.gold -= 50;
-                    } else {
-                        println !("골드가 부족합니다!");
-                    }
-                } else if input.trim() == "나가기" {
-                    println !("상점에서 나왔습니다.");
-                } else {
-                    println !("잘못된 입력입니다. 다시 입력해주세요.");
+                    println!("#------------------------------------#");
                 }
                 println !("");
-
-            }
-            // 특별 이벤트는 아직 생성 못함
-            Event::Special => {
-                println !("특별 이벤트를 발견했습니다!");
-                println !("특별 이벤트를 클리어했습니다.");
-                println !("");
-
             }
             Event::Rest => { // 휴식장소에서는 플레이어의 hp 회복
+                println!("");
+                println!("+------------------------------------+");
                 println !("휴식을 취할 수 있는 장소를 발견했습니다.");
                 println !("플레이어 HP: {}", player.hp);
                 player.hp += 20;
                 println !("체력이 20 회복되었습니다. 현재 체력: {}", player.hp);
+                println!("+------------------------------------+");
                 println !("");
-
             }
         }
         current_event += 1;
     }
     //마지막은 보스를 만난다.
+    println!("");
     println !("보스를 만났습니다!");
     let mut boss = loader::Monster{
         name: "Boss".to_string(),
@@ -178,7 +179,7 @@ fn main() {
             .unwrap();
 
         if input.trim() == "공격" {
-            let damage = rng.gen_range(10..=20);
+            let damage = rng.gen_range(100..=200);
             println !("플레이어가 {}의 피해를 입혔습니다!", damage);
             boss.hp -= damage;
 
@@ -199,7 +200,7 @@ fn main() {
             let success = rng.gen_bool(0.5);
             if success {
                 println !("플레이어가 도망쳤습니다.");
-                println !("");
+                println !("당신은 도망자입니다........");
                 return;
             } else {
                 println !("도망에 실패했습니다. 보스의 공격을 받습니다.");
