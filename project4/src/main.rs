2ea166fff7e978fe1b8885d56a9390286e92fb2e
Pilmo <pilmokim99@gmail.com>
2023-04-22 23:22:40 +0900

    ggez 튜토리얼로 연습 아직 기본 단계

--- project4/src/main.rs
+++ project4/src/main.rs
@@ -1,3 +1,17 @@
+use ggez::*;
+
+struct State {}
+
+impl ggez::event::EventHandler<GameError> for State {
+  fn update(&mut self, ctx: &mut Context) -> GameResult {
+      Ok(())
+  }
+  fn draw(&mut self, ctx: &mut Context) -> GameResult {
+      Ok(())
+  }
+}
+
 fn main() {
+    let state = State {};
     println!("Hello, world!");
 }
