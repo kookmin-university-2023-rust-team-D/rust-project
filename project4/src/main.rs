54ee49a0ce7bbbc0b57e531de2abab582a05acc4
Pilmo <pilmokim99@gmail.com>
2023-04-22 23:30:09 +0900

    add state code

--- project4/src/main.rs
+++ project4/src/main.rs
@@ -1,17 +1,23 @@
 use ggez::*;
 
-struct State {}
-
-impl ggez::event::EventHandler<GameError> for State {
-  fn update(&mut self, ctx: &mut Context) -> GameResult {
-      Ok(())
-  }
-  fn draw(&mut self, ctx: &mut Context) -> GameResult {
-      Ok(())
-  }
+struct State {
+    dt: std::time::Duration,
 }
 
+impl ggez::event::EventHandler<GameError> for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        self.dt = ctx.time.delta();
+        Ok(())
+    }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        println!("Hello ggez! dt = {}ns", self.dt.as_nanos());
+        Ok(())
+    }
+}
+
+
 fn main() {
-    let state = State {};
-    println!("Hello, world!");
+    let state = State {
+        dt: std::time::Duration::new(0, 0),
+    };
 }
